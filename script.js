e41ead9c83ee01b97f9fc56b63bd15639dd50e4e
Makokiko1 <aaronortizsanchez5@gmail.com>
2025-04-27 16:47:13 +0200

    24/04/2025

--- script.js
+++ script.js
@@ -126,28 +126,30 @@ function displayMenuItems(categoryId) {
     return;
   }
 
-  filteredItems.forEach((item) => {
-    const itemDiv = document.createElement("div");
-    itemDiv.classList.add("col-md-4", "mb-4");
-    itemDiv.innerHTML = `
-      <div class="card h-100 shadow">
-        <img
-          src="https://loremflickr.com/300/200/${encodeURIComponent(item.name)}"
-          class="card-img-top"
-          alt="${item.name}"
-        />
-        <div class="card-body d-flex flex-column">
-          <h5 class="card-title">${item.name}</h5>
-          <p class="card-text">${item.description}</p>
-          <p class="fw-bold text-success">$${parseFloat(item.price).toFixed(2)}</p>
-          <button class="btn btn-primary mt-auto" onclick="mostrarPersonalizacion(${item.id})">
-            Añadir
-          </button>
-        </div>
+ filteredItems.forEach((item) => {
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("col-md-4", "mb-4");
+  itemDiv.innerHTML = `
+    <div class="card h-100 shadow">
+      <img
+        src="${item.image_url}"
+        class="card-img-top"
+        alt="${item.name}"
+      />
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">${item.description}</p>
+        <p class="fw-bold text-success">$${parseFloat(item.price).toFixed(2)}</p>
+        <button class="btn btn-primary mt-auto"
+                onclick="mostrarPersonalizacion(${item.id})">
+          Añadir
+        </button>
       </div>
-    `;
-    menuContainer.appendChild(itemDiv);
-  });
+    </div>
+  `;
+  menuContainer.appendChild(itemDiv);
+});
+
 }
 
 // ========================
